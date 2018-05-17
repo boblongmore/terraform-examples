0deec42fea0f862644fa14f4d89895b33a2d1d25
boblongmore <robertlongmore@gmail.com>
2018-05-17 13:23:19 -0500

    added ansible provisioner

--- simple-ec2/ec2.tf
+++ simple-ec2/ec2.tf
@@ -2,14 +2,22 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-acc0f0d6"
+resource "aws_instance" "tf-example-01" {
+  count = 3
+  ami           = "ami-49ac3a36"
   instance_type = "t2.micro"
+  key_name = "web"
+  security_groups = ["web"]
+  tags {
+    Name = "tf-created"
+  }
+ provisioner "local-exec" {
+   command = "sleep 120; /etc/ansible/hosts; ansible-playbook site.yml"
+ }
 }
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
-output "ip" {
-  value = "${aws_eip.ip.public_ip}"
-}
-
+#resource "aws_eip" "ip" {
+#  instance = "${aws_instance.tf-example-01.*.id}"
+#}
+#output "ip" {
+#  value = "${(aws_eip.ip.public_ip}"
+#}
--- simple-ec2/variables.tf
+++ simple-ec2/variables.tf
@@ -1,4 +1,6 @@
-variable "region" {}
+variable "region" {
+    default = "us-east-1"
+}
 variable "amis" {
     type = "map"
 }
